c7d58cc553f650694a2b1fc0fc7629852dfe63a8
Joseph Lam <josephl0406@icloud.com>
2024-08-01 21:31:57 -0400

    Completed Assignment

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let nounInput = document.getElementById("noun");
 let verbInput = document.getElementById("verb");
 let adjectiveInput = document.getElementById("adjective");
 let submitBtn = document.getElementById("submit_button");
+let story = document.getElementById("story_result")
 
 function updateTitle(event) {
     event.preventDefault();
@@ -14,7 +15,11 @@ function updateTitle(event) {
 function submit(event){
     event.preventDefault();
     if (titleInput.value == "" || nounInput.value == "" || verbInput.value == "" || adjectiveInput.value == "" ){
-        window.alert("Please fill out all fields")
+        window.alert("Please fill out all fields");
+    }
+    else {
+       story.innerText = `Last night I ate a ${nounInput.value}, 
+       and today I had to ${verbInput.value}. What a ${adjectiveInput.value} day!`;
     }
 }
 
